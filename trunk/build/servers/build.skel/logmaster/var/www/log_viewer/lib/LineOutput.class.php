c6f86e1240cf35731e7c4369ad87172b108fafc3
Remi Broemeling <remi@nexopia.com>
2008-09-23 19:33:03 +0000

    * Add line numbers to output (simply for debugging information).

--- trunk/build/servers/build.skel/logmaster/var/www/log_viewer/lib/LineOutput.class.php
+++ trunk/build/servers/build.skel/logmaster/var/www/log_viewer/lib/LineOutput.class.php
@@ -10,6 +10,7 @@ class LineOutput
 		$offset = $line->get_offset();
 
 		echo "<div id='" . $offset . "' class='log_line " . $extra_classes . "'>";
+		echo '(' . number_format(self::$displayed_lines) . ') ';
 		echo "[<a href='?log=" . $_GET['log'] . "&offset=" . max(($offset - 8192), 0) . "&length=12288#" . $offset . "' name='" . $offset . "'>" . $offset .  "</a>] ";
 		echo (string)$line;
 		echo "</div>\n";
