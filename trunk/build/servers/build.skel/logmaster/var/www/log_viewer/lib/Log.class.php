31a57de9f9389ad7ae1259b55bf5a2559d74f850
Remi Broemeling <remi@nexopia.com>
2008-09-24 17:56:03 +0000

    * Make the default Log simpler and able to deal with simple "last message repeated"   syslog lines.

--- trunk/build/servers/build.skel/logmaster/var/www/log_viewer/lib/Log.class.php
+++ trunk/build/servers/build.skel/logmaster/var/www/log_viewer/lib/Log.class.php
@@ -19,40 +19,53 @@ class Log
 	public function __construct($line)
 	{
 		$fields = array();
-		$matches = array();
 
 		$this->line = $line;
 		$this->error_level = 'unknown';
 
 		# Regular Expression Map:
-		#	'(Sep 11 13:07:27) (10.0.3.8) (PHP error:) (.*)'
-		#	'(Sep 11 13:07:27) (10.0.3.8/10.0.0.16) (nexopia-parent - initializing:) (.*)'
-		if (preg_match('!^([a-z]{3} +\d+ +[0-9:]{8}) +([0-9./]+) +([^:]+:) +(.*)\n?$!i', $this->line, $fields))
+		#	'(Sep 11 13:07:27) (10.0.3.8) (.*)'
+		#	'(Sep 11 13:07:27) (10.0.3.8/10.0.0.16) (.*)'
+		if (preg_match('!^([a-z]{3} +\d+ +[0-9:]{8}) +([0-9./]+) +(.*)\n?$!i', $this->line, $fields))
 		{
-			# Alright, this appears to be a log in the default syslog format.
 			$this->syslog_date = strtotime($fields[1]);
 			$this->syslog_host = $fields[2];
-			$this->syslog_program = $fields[3];
 
-			# Regular Expression Map:
-			#  '([3443893/)(68.149.93.179]) (.*)'
-			if (preg_match('!(\[[0-9-]+/)([0-9.]+\]) +(.*)!', $fields[4], $matches))
-			{
-				$this->client_uid = $matches[1];
-				$this->client_ip = $matches[2];
-				$this->extra_data = $matches[3];
-			}
-			# Regular Expression Map:
-			#  '([68.149.93.179]) (.*)'
-			elseif (preg_match('!(\[[0-9.]+\]) +(.*)!', $fields[4], $matches))
-			{
-				$this->client_ip = $matches[1];
-				$this->extra_data = $matches[2];
-			}
-			else
-			{
-				$this->extra_data = $fields[4];
-			}
+			$this->extra_data = $fields[3];
+		}
+		else
+		{
+			# This is note a default syslog line... we really don't know what to do
+			# with it.
+			return;
+		}
+
+		# Regular Expression Map:
+		#	'(PHP error:) (.*)'
+		#	'(nexopia-parent - initializing:) (.*)'
+		if (preg_match('!^([^:]+:) +(.*)!', $this->extra_data, $fields))
+		{
+			$this->syslog_program = $fields[1];
+
+			$this->extra_data = $fields[2];
+		}
+
+		# Regular Expression Map:
+		#  '([3443893/)(68.149.93.179]) (.*)'
+		if (preg_match('!^(\[[0-9-]+/)([0-9.]+\]) +(.*)!', $this->extra_data, $fields))
+		{
+			$this->client_uid = $fields[1];
+			$this->client_ip = $fields[2];
+
+			$this->extra_data = $fields[3];
+		}
+		# Regular Expression Map:
+		#  '([68.149.93.179]) (.*)'
+		elseif (preg_match('!^(\[[0-9.]+\]) +(.*)!', $this->extra_data, $fields))
+		{
+			$this->client_ip = $fields[1];
+
+			$this->extra_data = $fields[2];
 		}
 	}
 
