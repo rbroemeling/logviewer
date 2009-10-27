2a9351f1e228964a7b3ad547185533c25e677fb1
Remi Broemeling <remi@nexopia.com>
2009-10-27 19:48:01 +0000

    * Performance enhancement: update create_link to be much more performant:   - do not use string concatenation (instead use an array of strings + implode)   - cache results of urlencode()   - do not urlencode() except for when necessary   - fetch log_timestamp() and get_offset() only once.

--- trunk/build/modules/srv-logmaster-dc/skeleton/var/www/log_viewer/lib/LineOutput.class.php
+++ trunk/build/modules/srv-logmaster-dc/skeleton/var/www/log_viewer/lib/LineOutput.class.php
@@ -16,24 +16,49 @@ class LineOutput
 
 	protected static function create_link($line)
 	{
+		static $encoded_environment = null;
+		static $encoded_language = null;
+
+		# Initialize the necessary convenience variables as necessary.
+		if (is_null($encoded_environment))
+		{
+			$encoded_environment = urlencode($_GET['environment']);
+		}
+		if (is_null($encoded_language))
+		{
+			$encoded_language = urlencode($_GET['language']);
+		}
+		$log_timestamp = $line->log_timestamp();
+		$log_offset = $line->get_offset();
+
+		# Create an array of query parameters and create the line identifier
+		# for this particular log line.
 		$query_params = array
 		(
-			'display_extended_filters' => 0,
-			'environment' => $_GET['environment'],
-			'language' => $_GET['language'],
-			'start_timestamp' => $line->log_timestamp() - 1,
-			'end_timestamp' => $line->log_timestamp() + 5
+			'display_extended_filters=0',
+			'environment=' . $encoded_environment,
+			'language=' . $encoded_language,
+			'start_timestamp=' . ($log_timestamp - 1),
+			'end_timestamp=' . ($log_timestamp + 5)
 		);
-		$line_identifier = $line->log_timestamp() . '.' . $line->get_offset();
+		$line_identifier = $log_timestamp . '.' . $log_offset;
 
-		$url = '?';
-		foreach ($query_params as $key => $value)
-		{
-			$url .= $key . '=' . urlencode($value) . '&';
-		}
-		$url = substr($url, 0, -1) . '#' . $line_identifier;
-
-		return "<a href='$url' id='" . $line_identifier . "' name='" . $line_identifier . "'>link</a>";
+		# Create the link by imploding an array of strings.  This is done
+		# rather than string concatenation for performance reasons (this
+		# is quicker).
+		$url_array = array
+		(
+			'<a href="?',
+			implode('&', $query_params),
+			'#',
+			$line_identifier,
+			'" id="',
+			$line_identifier,
+			'" name="',
+			$line_identifier,
+			'">link</a>'
+		);
+		return implode('', $url_array);
 	}
 }
 ?>
